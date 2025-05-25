365790ca6c46dbf32f3a34cb7be4c8ef84587f46
Rustam Gabdelislamov <rustamgabdelislamov911@gmail.com>
2025-05-25 21:47:52 +0500

    html ok

--- tests/test_read_xslx.py
+++ tests/test_read_xslx.py
@@ -22,14 +22,20 @@ def test_get_read_xlsx_ok(mock_get):
     ]
     path = 'data/transactions_excel.xlsx'
     result = get_read_xlsx(path)
-    assert result == [{'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z',
-                       'amount': '16210', 'currency_name': 'Sol', 'currency_code': 'PEN',
-                       'from': 'Счет 58803664561298323391', 'to': 'Счет 397456605634566193',
-                       'description': 'Перевод организации'},
-                      {'id': '3598919', 'state': 'EXECUTED', 'date': '2020-12-06',
-                       'amount': '29740', 'currency_name': 'Peso', 'currency_code': 'COP',
-                       'from': 'Discover 3172601889670065', 'to': 'Discover 07204283846946',
-                       'description': 'Перевод с карты на карту'}]
+    assert result == [
+        {
+            'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z',
+            'amount': '16210', 'currency_name': 'Sol', 'currency_code': 'PEN',
+            'from': 'Счет 58803664561298323391', 'to': 'Счет 39745660563456619397',
+            'description': 'Перевод организации'
+        },
+        {
+            'id': '3598919', 'state': 'EXECUTED', 'date': '2020-12-06T23:00:58Z',
+            'amount': '29740', 'currency_name': 'Peso', 'currency_code': 'COP',
+            'from': 'Discover 3172601889670065', 'to': 'Discover 0720428384694643',
+            'description': 'Перевод с карты на карту'
+        }
+    ]
 
 
 def test_read_csv_path_no():
